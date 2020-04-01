dfd54a11e2eb7d07e2c225a1d0e506b5f89afb8f
hua <huald@ibuaa.com>
2020-04-01 17:13:20 +0800

    去除swagger

--- query-demo/src/main/java/com/hld/vo/NewsVO.java
+++ query-demo/src/main/java/com/hld/vo/NewsVO.java
@@ -1,10 +1,7 @@
 package com.hld.vo;
 
-import com.techcomer.common.mybatisplus.entity.BaseEntity;
-import com.techcomer.common.mybatisplus.query.annotations.TableFiledInfo;
-import com.techcomer.common.mybatisplus.query.annotations.TableRelations;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import com.hld.query.annotations.TableFiledInfo;
+import com.hld.query.annotations.TableRelations;
 import lombok.Data;
 
 /**
@@ -14,23 +11,18 @@ import lombok.Data;
  * @date 2019/8/20
  */
 @Data
-@ApiModel
 @TableRelations(relation = "t_news T left join sys_user A on A.id = T.edit_user_id", mTableName = "t_news")
-public class NewsVO extends BaseEntity {
+public class NewsVO {
 
     @TableFiledInfo()
-    @ApiModelProperty("新闻标题")
     private String title;
 
     @TableFiledInfo()
-    @ApiModelProperty("新闻内容")
     private String content;
 
     @TableFiledInfo()
-    @ApiModelProperty("新闻编辑人ID")
     private Long editUserId;
 
-    @ApiModelProperty("新闻编辑人名字")
     @TableFiledInfo(tableName = "sys_user", tableAlias = "A", filedName = "username")
     private String editUserName;
 
