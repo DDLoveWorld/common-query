48dbe5253fadef99912e805d19e0c34454e1c63b
hua <huald@ibuaa.com>
2020-12-08 15:41:01 +0800

    1、添加Service自定义查询接口（帮助实际生产中进行查询业务拦截） 2、修改版本号1.3.3-》1.3.4 3、去除无效引用

--- src/main/java/com/hld/query/test/QueryTest.java
+++ src/main/java/com/hld/query/test/QueryTest.java
@@ -1,14 +1,12 @@
 package com.hld.query.test;
 
-import com.hld.query.params.OrderType;
-import com.hld.query.test.vo.SysUserVO;
 import com.hld.query.params.IFilter;
 import com.hld.query.params.IOrderBy;
-
-import java.util.*;
-
+import com.hld.query.params.OrderType;
 import com.hld.query.params.QueryOptions;
-import com.hld.query.util.QueryUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author huald
@@ -48,44 +46,4 @@ public class QueryTest {
         System.out.println(options.toString());
     }
 
-    private static void testSql() {
-        QueryOptions options = new QueryOptions();
-
-        List<String> columns = new ArrayList<>(12);
-        columns.add("id");
-        columns.add("deptName");
-
-        List<IFilter> filters = new ArrayList<>(12);
-
-        IFilter iFilter = new IFilter();
-        List<Object> l = new ArrayList<>();
-        l.add("sdids");
-        iFilter.setFilterName("deptName").setFilterValue(l);
-
-        filters.add(iFilter);
-
-
-        options.setColumns(columns).setFilters(filters).setCurPage(null).setLimit(null);
-
-        List<String> groups = new ArrayList<>(12);
-        groups.add("id");
-        groups.add("deptName");
-
-        List<IOrderBy> orders = new ArrayList<>(12);
-        IOrderBy orderBy = new IOrderBy();
-        orderBy.setOrderByName("id").setOrderByType(OrderType.ASC);
-        orders.add(orderBy);
-
-
-        options.setOrderBys(orders).setGroupBys(groups);
-
-        String s = commonQuery(options);
-        System.out.println("test sql = " + s);
-    }
-
-    public static String commonQuery(QueryOptions params) {
-//        QueryUtils<SysUserVO> queryUtils = new QueryUtils<>();
-//        return queryUtils.testSql2(params, SysUserVO.class, null);
-        return QueryUtils.testSql(params, SysUserVO.class, null);
-    }
 }
